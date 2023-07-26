06f20a6e7eccbb19ee7045a6dc51ee20b0a6c316
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-07-25 19:59:29 -0600

    Implemented child components to simplify App
    -Assignment: Time to Practice: React & Component Basics Done
    
    - Created Expenses component
    
    - Created ExpenseDate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ExerciseComponent from "./Components/ExerciseComponent";
 import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 
 function App() {
   const expenses = [
@@ -28,28 +29,33 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
 
-      {/* <ExerciseComponent></ExerciseComponent> */}
+      <Expenses expense={expenses}></Expenses>
+      {/* <Expenses expense={expenses[1]}></Expenses>
+      <Expenses expense={expenses[2]}></Expenses>
+      <Expenses expense={expenses[3]}></Expenses> */}
 
-      <ExpenseItem
+      {/* <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      /> */}
+
+      {/* <ExerciseComponent></ExerciseComponent> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import AppCC5 from "./AppCC5";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppCC5 />);
+root.render(<App />);
