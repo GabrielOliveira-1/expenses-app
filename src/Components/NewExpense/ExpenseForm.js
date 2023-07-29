4993b5d3964c2feb069e7e6ee5113d26dd265655
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-07-29 12:58:39 -0600

    Update ExpenseForm.js
    - Implement state changes with handlers and useState

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const [enteredDate, setEnteredDate] = useState("");
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
   return (
     <form>
       <div>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" />
+            <input type="text" onChange={titleChangeHandler} />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" />
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={amountChangeHandler}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2021-08-18" max="2101-08-18" />
+            <input
+              type="date"
+              min="2021-08-18"
+              max="2101-08-18"
+              onChange={dateChangeHandler}
+            />
           </div>
         </div>
       </div>
