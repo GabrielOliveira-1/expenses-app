9a8ac003adda5705d352772e2b8cf4aa7386d35d
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-07-30 10:30:30 -0600

    Update ExpenseForm.js
    - Add onSubmit handler inside form tag

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,43 +2,60 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (e) => {
-    // setEnteredTitle(e.target.value);
+    setEnteredTitle(e.target.value);
 
-    setUserInput({
-      ...userInput,
-      enteredTitle: e.target.value,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: e.target.value,
+    // });
+
+    // This is a better use for when updating states based on previous states
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: e.target.value };
+    // });
   };
-  const amountChangeHandler = (e) => {
-    // setEnteredAmount(e.target.value);
 
-    setUserInput({
-      ...userInput,
-      enteredAmount: e.target.value,
-    });
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: e.target.value,
+    // });
   };
   const dateChangeHandler = (e) => {
-    // setEnteredDate(e.target.value);
+    setEnteredDate(e.target.value);
 
-    setUserInput({
-      ...userInput,
-      enteredDate: e.target.value,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: e.target.value,
+    // });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault(); // This prevents the page to reload, among other default behaviors that the submit button triggers the form to do
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
         <div className="new-expense__controls">
           <div className="new-expense__control">
